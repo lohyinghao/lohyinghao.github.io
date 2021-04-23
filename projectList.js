09d566309b0dd239fcb89b5eb6872eeb68628831
lohyinghao <lohyinghao91@gmail.com>
2021-04-23 21:11:38 +0800

    add new project

--- projectList.js
+++ projectList.js
@@ -1,19 +1,27 @@
 const data = [
   {
+    projectName: 'Prophet League Dashboard',
+    previewImage: 'prophetLeague.png',
+    demoLink: 'https://prophetleague.herokuapp.com/',
+    repoLink: 'https://github.com/lohyinghao/fpl-react',
+    description: `A web application to keep track of the contributions each participant needs to make to the prize pool as part of their Fantasy Permier League Competition.
+      <br>
+      <br>
+      This project is built using the following technologies:
+      <br>
+      <strong>ReactJS</strong> - Web framework<br>
+      <strong>Rechart</strong> - React based charting library<br>
+      <strong>Ag-grid</strong> - Datagrid framework<br>
+      <strong>Github Actions</strong> - CI/CD pipeline for automated deployment to heroku<br>
+      `,
+  },
+  {
     projectName: 'Loan Management System',
     previewImage: 'loanManagementSystem.png',
     demoLink: 'https://loanmanagementsystem.herokuapp.com/',
     repoLink: 'https://github.com/lohyinghao/LoanManagementSystem',
     description:
-      'A web application built using <strong>Angular</strong> for a fictional loan company. Utilizing this app, staff will be able to submit customer loans application for interest rate calculations and approval.'
-  },
-  {
-    projectName: 'Prize Pool Tracker',
-    previewImage: 'prizePoolTracker.png',
-    demoLink: 'https://theprophetleague.herokuapp.com/',
-    repoLink: 'https://github.com/lohyinghao/Fpl-Prize-Tracker',
-    description:
-      'A web application built using <strong>AngularJS</strong> to keep track of four friends’ weekly contribution into the Prize Pool as part of their Fantasy Premier League competition. This frontend application makes use of the REST API provided by the official Fantasy Premier League site. A nodeJS backend is setup to hit the Fantasy Premier League API to bypass CORS restriction.'
+      'A web application built using <strong>Angular</strong> for a fictional loan company. Utilizing this app, staff will be able to submit customer loans application for interest rate calculations and approval.',
   },
   {
     projectName: 'Json Viewer',
@@ -21,6 +29,14 @@ const data = [
     demoLink: 'https://lohyinghao.github.io/json-viewer/',
     repoLink: 'https://github.com/lohyinghao/json-viewer',
     description:
-      'A JSON Viewer based on <strong>React</strong> and <strong>React Context API</strong>.'
-  }
+      'A JSON Viewer based on <strong>React</strong> and <strong>React Context API</strong>.',
+  },
+  {
+    projectName: 'Prize Pool Tracker',
+    previewImage: 'prizePoolTracker.png',
+    demoLink: 'https://theprophetleague.herokuapp.com/',
+    repoLink: 'https://github.com/lohyinghao/Fpl-Prize-Tracker',
+    description:
+      'A web application built using <strong>AngularJS</strong> to keep track of four friends’ weekly contribution into the Prize Pool as part of their Fantasy Premier League competition. This frontend application makes use of the REST API provided by the official Fantasy Premier League site. A nodeJS backend is setup to hit the Fantasy Premier League API to bypass CORS restriction.',
+  },
 ];
